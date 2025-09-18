f001ed17323ad7a370c35a79b21dfd18dc6a9852
jukaveka <juhokavekari@hotmail.com>
2025-09-18 20:32:48 +0300

    BMI app - Create HTTP GET 'Hello* -endpoint

--- bmi-app/index.ts
+++ bmi-app/index.ts
@@ -1,2 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
