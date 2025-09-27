49dad1cd01e91f109063fe3182f9856960aa4b36
jukaveka <juhokavekari@hotmail.com>
2025-09-27 18:42:24 +0300

    Patientor BE - Handle new patient endpoint errors with Zod, infer NewPatient from NewPatientSchema

--- patientor/backend/src/routes/patientRouter.ts
+++ patientor/backend/src/routes/patientRouter.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { Response } from "express";
+import { z } from "zod";
 import { NonSensitivePatient } from "../types";
 import patientService from "../services/patientService";
-import { parseErrorMessage } from "../utils/parseValue";
 import { NewPatientSchema } from "../utils/toPatient";
 
 const router = express.Router();
@@ -11,7 +11,7 @@ router.get("/", (_req, res: Response<NonSensitivePatient[]>) => {
   return res.send(patientService.getNonSensitivePatients());
 });
 
-router.post("/", (req, res: Response<NonSensitivePatient | string>) => {
+router.post("/", (req, res) => {
   try {
     const validatedPatient = NewPatientSchema.parse(req.body);
 
@@ -19,7 +19,11 @@ router.post("/", (req, res: Response<NonSensitivePatient | string>) => {
 
     return res.send(addedPatient);
   } catch (error) {
-    return res.status(400).send(parseErrorMessage(error));
+    if (error instanceof z.ZodError) {
+      return res.status(400).send({ error: error.issues });
+    } else {
+      return res.status(400).send({ error: "unknown error " });
+    }
   }
 });
 
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,3 +1,6 @@
+import { z } from "zod";
+import { NewPatientSchema } from "./utils/toPatient";
+
 export interface Diagnosis {
   code: string;
   name: string;
@@ -15,7 +18,7 @@ export interface Patient {
 
 export type NonSensitivePatient = Omit<Patient, "ssn">;
 
-export type NewPatient = Omit<Patient, "id">;
+export type NewPatient = z.infer<typeof NewPatientSchema>;
 
 export enum Gender {
   Male = "male",
