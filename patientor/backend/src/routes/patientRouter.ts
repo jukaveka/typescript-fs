a7acaf3481ceb262e994236ee9374d199ed988c6
jukaveka <juhokavekari@hotmail.com>
2025-10-13 16:51:06 +0300

    Patientor BE - Add error handling to posting new patient entries

--- patientor/backend/src/routes/patientRouter.ts
+++ patientor/backend/src/routes/patientRouter.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { NonSensitivePatient, NewPatientSchema } from "../types/patientTypes";
 import patientService from "../services/patientService";
 import { parseNewEntry } from "../utils/parseValue";
+import { NewEntry } from "../types/entryTypes";
 
 const router = express.Router();
 
@@ -36,11 +37,19 @@ router.post("/", (req, res) => {
 router.post("/:id/entries", (req, res) => {
   const patientId = req.params.id;
 
-  const entry = parseNewEntry(req.body);
+  try {
+    const entry: NewEntry = parseNewEntry(req.body);
 
-  const addedEntry = patientService.addPatientEntry(patientId, entry);
+    const addedEntry = patientService.addPatientEntry(patientId, entry);
 
-  return res.send(addedEntry);
+    return res.send(addedEntry);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).send({ error: error.message });
+    } else {
+      return res.status(400).send({ error: "unknown error " });
+    }
+  }
 });
 
 export default router;
